fcba21e467d3184b7d3e51d4eaec4a33d95be1bf
ecoal95 <ecoal95@gmail.com>
2015-05-11 19:17:13 +0200

    Update view

--- etc/sql/common.mk
+++ etc/sql/common.mk
@@ -1,7 +1,7 @@
 target:
 	@mkdir -p $@
 
-target/global.sql: src/schema.sql target/triggers.sql src/seeds.sql
+target/global.sql: src/schema.sql target/triggers.sql src/view.sql src/seeds.sql
 	cat $^ > $@
 
 target/triggers.sql: src/derived_and_triggers.sql target/auto_triggers.sql
--- etc/sql/src/view.sql
+++ etc/sql/src/view.sql
@@ -1,6 +1,6 @@
 --View 'questions_counter'
 CREATE OR REPLACE VIEW questions_counter AS
-    SELECT subjects_teachers.teacher_id, themes.subject_id, themes.id,
+    SELECT subjects_teachers.teacher_id, themes.subject_id, themes.id AS theme_id,
         -- We want to count all the themes, not just those who have at least one question
         (SELECT COUNT(qt.question_id) FROM questions_themes qt WHERE qt.theme_id = themes.id) AS question_count
             FROM subjects_teachers, themes
